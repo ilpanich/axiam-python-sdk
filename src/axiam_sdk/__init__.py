4c276de4628aafd7a285dbdde8f15f4f42c08a0b
Emanuele Panigati <ilpanich@gmail.com>
2026-08-23 18:52:00 +0200

    chore(release): prepare axiam-python-sdk 1.0.0-alpha40

--- src/axiam_sdk/__init__.py
+++ src/axiam_sdk/__init__.py
@@ -66,7 +66,7 @@ from axiam_sdk._webauthn import (
     webauthn_request_json,
 )
 
-__version__ = "1.0.0a39"
+__version__ = "1.0.0a40"
 
 from axiam_sdk._decision_memo import DecisionMemo
 from axiam_sdk._telemetry import (
